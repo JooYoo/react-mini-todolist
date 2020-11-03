12720fe8742181722796ca7e4f5e32ec848c471b
JooYoo <jooyoo@outlook.com>
2020-11-03 20:11:19 +0100

    feat: retrieve todos locally

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const initTodos = [
+    {
+      text: "play Switch",
+      isCompleted: false
+    },
+    {
+      text: "play xBox",
+      isCompleted: false
+    },
+    {
+      text: "play PlayStation",
+      isCompleted: false
+    }
+  ];
+
+  const [todos, setTodos] = useState(initTodos);
+
   return (
     <div className="App">
-      <h1>React Hooks: TodoList</h1>
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <div key={index}>
+            {index}
+            {todo.text}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
